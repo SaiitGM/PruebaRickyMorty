9269337c4ae1c0f699246b4f7a0fb49ddcffcfb5
Saiit <Saiit@SAIITGM>
2025-07-15 16:26:00 -0500

    Agregar archivos de proyecto.

---
+++ RickyMorty/Modelos/Personajes.cs
@@ -0,0 +1,20 @@
+﻿namespace RickyMorty.Modelos {
+    public class PersonajeRespuesta {
+        public Info info { get; set; }
+        public List<Personaje> results { get; set; }
+    }
+
+    public class Info {
+        public int count { get; set; }
+        public int pages { get; set; }
+    }
+
+    public class Personaje {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string status { get; set; }
+        public string species { get; set; }
+        public string image { get; set; }
+    }
+}
+
---
+++ RickyMorty/Servicios/PersonajeServicios.cs
@@ -0,0 +1,17 @@
+﻿using System.Net.Http.Json;
+using RickyMorty.Modelos;
+
+namespace RickyMorty.Servicios {
+    public class PersonajeServicios {
+        private readonly HttpClient _http;
+
+        public PersonajeServicios(HttpClient http) {
+            _http = http;
+        }
+
+        public async Task<List<Personaje>> GetCharactersAsync() {
+            var result = await _http.GetFromJsonAsync<PersonajeRespuesta>("https://rickandmortyapi.com/api/character");
+            return result.results;
+        }
+    }
+}
